dcc702aeb4cfc03a8b729a7b1239da02ca1b65a6
Eric Marques <emarques3@gmail.com>
2021-01-17 12:29:21 -0600

    Adding code documentation to HOC

--- src/ReactUtils/withSplit.tsx
+++ src/ReactUtils/withSplit.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useSplit } from './useSplit';
 import type { WithSplitProps } from './types';
 
+/**
+ * The withSplit HOC
+ * @param Component the component to be wrapped by the HOC, which will receive the new `splitFlagOn` prop
+ * @param split The split (flag) to connect/listen to
+ * @param defaultValue The default value to be applied if/when Split client is not ready
+ * @returns The wrapped component
+ */
 export const withSplit = <T extends Record<string, unknown>>(
   Component: React.ComponentType<T & WithSplitProps>,
   split: string,
